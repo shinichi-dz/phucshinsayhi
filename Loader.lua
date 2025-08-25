dbdb736f870c16f87161a535772a3761a2c94dcd
Shinichi <nxuan1934@gmail.com>
2025-08-25 19:43:10 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet(""))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/shinichi-dz/phucshinsayhi/refs/heads/main/loader.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
