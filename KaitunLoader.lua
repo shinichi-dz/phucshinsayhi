edd43d8d8deab310f87cf6c9efe7269cc837357d
Shinichi <nxuan1934@gmail.com>
2025-08-26 20:27:26 +0700

    Delete KaitunLoader.lua

--- KaitunLoader.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/shinichi-dz/phucshinsayhi/refs/heads/main/KaitunLoader.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
-    end
-end
